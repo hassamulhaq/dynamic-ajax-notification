6408b6e90068560d1931b2df17fe69e8fd824895
hassamulhaq <developer.hassam@gmail.com>
2023-12-11 21:27:36 +0500

    Dynamic ajax file and toastify assets added

---
+++ public/assets/js/toastify-constants.js
@@ -0,0 +1,3 @@
+export const TOASTIFY_ERROR_COLOR = 'linear-gradient(to right, rgb(176 0 19), rgb(168 54 54))'
+export const TOASTIFY_SUCCESS_COLOR = 'linear-gradient(to right, rgb(0 176 80), rgb(54 168 89))'
+export const TOASTIFY_INFO_COLOR = 'linear-gradient(to right, rgb(5 56 162), rgb(63 54 168))'
